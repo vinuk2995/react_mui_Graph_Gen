1e2a065a152d516aa38fe286da34fff1f0a33f6a
vinuk2995 <35198687+vinuk2995@users.noreply.github.com>
2018-11-13 11:04:19 +0530

    Add files via upload
    reactjs graph generation using rest api

---
+++ graph/src/components/Chart.js
@@ -0,0 +1,23 @@
+import React, {component} from 'react' ;
+import {Bar,Line, pie} from 'react';
+
+class Chart extends component{
+
+    render()
+    {
+        return (
+            <div className="chart">
+            
+            Chart Component
+            
+            </div>
+
+
+
+        )
+    }
+
+
+
+}
+export default Chart;
